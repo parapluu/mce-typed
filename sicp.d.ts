bd18483d72a671bf053c10380a9efd8203c0a359
Alexander Björkman <97904317+sashabjorkman@users.noreply.github.com>
2023-02-13 16:48:56 +0100

    Move sicp.d.ts definitions into the declare module
    To get proper type-checking, we must "augment" the `sicp` import as opposed to simply defining an alternate version of it.

--- sicp.d.ts
+++ sicp.d.ts
@@ -1,27 +1,26 @@
-declare module 'sicp';
+declare module 'sicp' {
+    export type Pair<H, T> = [H, T];
+    export type List<T> = [T, List<T>] | null;
 
-type Pair<H, T> = [H, T];
-type List<T> = [T, List<T>] | null;
-
-declare function is_boolean(value: any): boolean;
-declare function set_head<H, T>(pair: Pair<H, T>, elem: H): Pair<H, T>;
-declare function is_pair(value: any): boolean;
-declare function list_ref(list: any, index: number): any;
-declare function apply_in_underlying_javascript(f: any, args: any): any;
-declare function pair<H,T>(head: H, tail: T): Pair<H, T>;
-declare function stringify(value: any): string;
-declare function is_null(value: any): boolean;
-declare function error(...args: [any]): any;
-declare function math_abs(value: number): number;
-declare const math_PI: number;
-declare const math_E: number;
-declare function display(...x: any[]): any;
-declare function map<H, T>(fun: (from: H) => T, list: List<H>): List<T>;
-declare function accumulate(x: any): any;
-declare function parse(x: any): any;
-declare function append<T>(a: List<T>, b: List<T>): List<T>;
-declare function head<H, T>(pair: Pair<H, T>): H;
-declare function list<T>(...elems: [T]): List<T>;
-declare function tail<H, T>(pair: Pair<H, T>): T;
-
+    export function is_boolean(value: any): boolean;
+    export function set_head<H, T>(pair: Pair<H, T>, elem: H): Pair<H, T>;
+    export function is_pair(value: any): boolean;
+    export function list_ref(list: any, index: number): any;
+    export function apply_in_underlying_javascript(f: any, args: any): any;
+    export function pair<H,T>(head: H, tail: T): Pair<H, T>;
+    export function stringify(value: any): string;
+    export function is_null(value: any): boolean;
+    export function error(...args: [any]): any;
+    export function math_abs(value: number): number;
+    export const math_PI: number;
+    export const math_E: number;
+    export function display(...x: any[]): any;
+    export function map<H, T>(fun: (from: H) => T, list: List<H>): List<T>;
+    export function accumulate(x: any): any;
+    export function parse(x: any): any;
+    export function append<T>(a: List<T>, b: List<T>): List<T>;
+    export function head<H, T>(pair: Pair<H, T>): H;
+    export function list<T>(...elems: [T]): List<T>;
+    export function tail<H, T>(pair: Pair<H, T>): T;
+}
 
